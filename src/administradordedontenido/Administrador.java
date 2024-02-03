826ac95dbbd6d97a82364970696eada2f73dab87
ricar <ricar@DESKTOP-SRR05PO>
2024-02-03 02:44:35 -0800

    Ejercicio Encapsulamiento

--- src/administradordedontenido/Administrador.java
+++ src/administradordedontenido/Administrador.java
@@ -1,28 +1,61 @@
 package administradordedontenido;
 
+import java.util.*;
 public class Administrador {
     private Archivo[] almacen;
+    boolean hayArchivos = false;
+    int contArchivos = 0;
+    Scanner sc = new Scanner(System.in);
 
     public Administrador() {
         
         almacen = new Archivo[10];
     }
     
-    public void registrarArchivo(){
-        almacen[0] = new Archivo("imagen", "importante.jpg", 35, "/root", "null", 1);
-        almacen[1] = new Archivo("video", "cars.mp4", 680, "/home", "null", 2);
+    public void registrarArchivoX(){
+        hayArchivos = true;
+        almacen[0] = new Archivo("imagen", "importante.jpg", 35, "/root", "n/a", 1);
+        almacen[1] = new Archivo("video", "cars.mp4", 680, "/home", "n/a", 2);
         almacen[2] = new Archivo("cpp", "main.cpp", 35, "/home/documents", "C++", 3);
     }
     
-    public void leerArchivos(){
-        System.out.println("Nombre de la primera persona: " + almacen[0].getTipoContenido());
-        System.out.println("Edad de la segunda persona: " + almacen[1].getNombre());
-        System.out.println("Nombre de la primera persona: " + almacen[2].getLenguaje());
+    public void registrarArchivo(){
+        hayArchivos = true;
+        System.out.println("Ingresa 1 si el archivo es de programación.");
+        System.out.println("Ingresa cualquier otro numero si no aplica.");
+        int progra = sc.nextInt();
+        sc.nextLine(); // Consumir el salto de línea pendiente
+        System.out.println("\n");
+        String lenguaje = "";
+        if(progra == 1){
+            System.out.print("Ingresa el lenguaje del archivo: ");
+            lenguaje = sc.nextLine();
+        }else{
+            lenguaje = "n/a";
+        }
+        System.out.print("Ingresa el tipo de archivo: ");
+        String tipo = sc.nextLine();
+        System.out.print("Ingresa el nombre del archivo: ");
+        String nombre = sc.nextLine();
+        System.out.print("Ingresa el tamano del archivo: ");
+        int tamano = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Ingresa el path: ");
+        String path = sc.nextLine();
+        almacen[contArchivos] = new Archivo(tipo, nombre, tamano, path, lenguaje, contArchivos);
+        contArchivos++;
     }
     
-    public void leerArchivosEnOrden(){
-        for (int i=0; i<3; i++){
-            System.out.println("Nombre de la primera persona: " + almacen[i].getTipoContenido());
+    public void mostrarRegistros() {
+        if (hayArchivos) {
+            System.out.printf("%-5s %-12s %-30s %-10s %-30s %-10s%n", "ID", "TIPO", "NOMBRE", "TAMANO", "PATH", "LENGUAJE");
+            for (int i = 0; i < contArchivos; i++) {
+                System.out.printf("%-5s %-12s %-30s %-10s %-30s %-10s%n",
+                        almacen[i].getLlave(), almacen[i].getTipoContenido(), almacen[i].getNombre(),
+                        almacen[i].getSize(), almacen[i].getPath(), almacen[i].getLenguaje());
+            }
+        } else {
+            System.out.println("No hay archivos registrados aún...");
         }
     }
 
--- src/administradordedontenido/AdministradorDeDontenido.java
+++ src/administradordedontenido/AdministradorDeDontenido.java
@@ -4,13 +4,38 @@ package administradordedontenido;
  *
  * @author ricar
  */
+import java.util.*;
 public class AdministradorDeDontenido {
 
     public static void main(String[] args) {
-        Administrador administrador = new Administrador();
+        Administrador admin = new Administrador();
+        Scanner sc = new Scanner(System.in);
         
-        administrador.registrarArchivo();
-        administrador.leerArchivosEnOrden();
+        System.out.println("ADMINISTRADOR DE CONTENIDO DIDÁCTICO");
+        int op = 0;
+        do{
+            System.out.println("........................");
+            System.out.println("1. Registro de archivos.");
+            System.out.println("2. Mostrar registros.");
+            System.out.println("0. Salir.");
+            System.out.print("Selecciona una opción: ");
+            op = sc.nextInt();
+            switch (op){
+                case 0:
+                    System.out.println("\nGracias!!");
+                    break;
+                case 1:
+                    System.out.println("REGISTRO DE ARCHIVOS");
+                    admin.registrarArchivo();
+                    break;
+                case 2:
+                    System.out.println("LECTURA DE ARCHIVOS");
+                    admin.mostrarRegistros();
+                    break;
+                default:
+                    System.out.println("\nOpcion no valida, vuela a intentarlo...\n");
+            }
+        }while(op!=0);
     }
     
 }
